312c92560b1495c25ae57608c32000967681da62
Florence <monnaie.florence@gmail.com>
2021-01-14 10:43:03 +0100

    exo8 done

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import './App.css';
 
 class App extends Component {
 
-  loguer = () => {
+  state = {
+    number : 1
+  };
+
+
+  loguer = (e) => {
     console.log('Bonjour');
+    e.target.parentElement.classList.toggle('bg');
+    e.target.parentElement.firstElementChild.classList.toggle('white');
+  };
+
+  hover = (e) => {
+    console.log(e.target.textContent);
   };
 
   render() {
+
+    console.log(this.state.number);
+
     return (
       <div className="App">
-        <h1>Exercice 8</h1>
+        <h1 onMouseOver={this.hover}>Exercice 8</h1>
         <button onClick={this.loguer}>Clique ici</button>
       </div>
     )
